ba55de54957c23e605cba236e2b6ba5f4d392c1f
kchia <kchia87@gmail.com>
2019-09-24 00:25:15 -0400

    add feedback

---
+++ feedback.md
@@ -0,0 +1,30 @@
+# Feedback
+
+## Code Quality - Performing
+
+Code is well-formatted, well-named, and contains few style / quality issues
+
+-   Also, make sure to remove any unused code from your HTML/JS/CSS files and also indent your code properly!
+
+## Technical Requirements - Performing
+
+Renders in browser with no major errors and seperates concerns into distinct HTML, CSS, and JavaScript files.
+
+-   The game works well in the browser! Great job using media queries to make your game responsive.
+
+## Creativity and Interface - Excelling
+
+The app displays a personal spin or creative element through its written language, functionality, purpose, and/or CSS.
+
+-   I love the design of your game, especially with the click-and-flip-a-card feature! I thought the theme you chose was really cool and interesting :)
+
+## Presentation - Performing
+
+Application has functional and creative interface and a readme with relevant information about the application.
+
+-   Your readme is informative! I thought the format made sense. However, some of your header formatting seems to be off (e.g., ##Motivation header rendering incorrectly in your readme). Fixing your headings so that they render properly would make the flow of your readme much easier to follow. Take a look at Jared's readme for inspiration: https://github.com/jaredsmorgan/astronomy-memory-game
+
+## Additional Feedback
+
+-   Take a look at some of my inline comments on your code.
+-   Take a look at how I've used the prettier plugin to automatically clean up your code formatting!
--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+// HC: great job adding comments to describe your code!
 //NIGHT MODE
 let night = document.querySelector('.night');
 let nightClick = 0;
 function lightsOff() {
     nightClick++;
     if (nightClick % 2 === 0) {
-        document.body.style.backgroundColor = "white";
-    }
-    else {
-        document.body.style.backgroundColor = "#353c51";
+        document.body.style.backgroundColor = 'white';
+    } else {
+        document.body.style.backgroundColor = '#353c51';
     }
 }
-night.addEventListener('click', lightsOff)
+night.addEventListener('click', lightsOff);
 //////////////////////////////
 //SCORE
 let scoreNum = 0;
@@ -19,10 +19,10 @@ let score = document.querySelector('.score');
 
 ///////BEGIN BUTTON
 let beginBtn = document.querySelector('.begin');
-let modal = document.querySelector('.modal')
+let modal = document.querySelector('.modal');
 
 function begin() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
 }
 beginBtn.addEventListener('click', begin);
 
@@ -30,68 +30,64 @@ beginBtn.addEventListener('click', begin);
 let numberOfClicks = 0;
 let firstCard;
 let secondCard;
-let board = document.querySelector('.card-area')
+let board = document.querySelector('.card-area');
 
-
+// HC: Consider breaking up the logic inside this disappearingAct function into smaller helper function calls
 /////Listen to click
 function disappearingAct(e) {
     if (e.target.classList.contains('faceCard')) {
-        let timer = 0
-        numberOfClicks++
+        let timer = 0;
+        numberOfClicks++;
 
         e.target.src = e.target.dataset.img;
         if (numberOfClicks % 2 == 1) {
             //pass the odd clicks into first card
             firstCard = e.target;
-        }
-        else if (numberOfClicks % 2 === 0) {
+        } else if (numberOfClicks % 2 === 0) {
             //pass the even clicks into second card
             //compare if name is the same
             secondCard = e.target;
             if (firstCard.name === secondCard.name) {
                 //add a pt
                 scoreNum += 1;
-                number += 1
-                score.innerText = scoreNum
+                number += 1;
+                score.innerText = scoreNum;
                 //UPDATE SCORE ON DOM
                 if (scoreNum === 6) {
-                    win.style.display = "block"
+                    win.style.display = 'block';
                 }
-            }
-            else {
+            } else {
                 setTimeout(() => {
-
                     //flip cards back over
                     //use src to flip back over
-                    firstCard.src = "img/jcg_bg_red@3x.png";
+                    firstCard.src = 'img/jcg_bg_red@3x.png';
                     //use src to flip back over
-                    secondCard.src = "img/jcg_bg_red@3x.png";
-                }, 1000)
+                    secondCard.src = 'img/jcg_bg_red@3x.png';
+                }, 1000);
             }
         }
         function playAgain() {
-            win.style.display = "none";
+            win.style.display = 'none';
             location.reload();
-
         }
         againBtn.addEventListener('click', playAgain);
 
         function close() {
-            win.style.display = "none";
+            win.style.display = 'none';
         }
-        closeBtn.addEventListener('click', close)
+        closeBtn.addEventListener('click', close);
         let modalText = document.querySelector('.winner-text');
         function getTime(timer) {
-            timer++
+            timer++;
         }
-        setInterval(getTime, 1000)
+        setInterval(getTime, 1000);
 
-        modalText.innerHTML = `It took ${numberOfClicks} moves to complete this challenge. Great Job!`
+        modalText.innerHTML = `It took ${numberOfClicks} moves to complete this challenge. Great Job!`;
     }
 }
 board.addEventListener('click', disappearingAct);
 //////Listen for click
 ////////////////AGAIN
-let closeBtn = document.querySelector('.close')
+let closeBtn = document.querySelector('.close');
 let againBtn = document.querySelector('.play-again');
-let win = document.querySelector('.modal-again')
+let win = document.querySelector('.modal-again');
